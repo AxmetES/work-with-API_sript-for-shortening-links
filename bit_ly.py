1f4b3d7c792b0e970dbe543bf8f7f315119ecaa7
AxmetES <axmet-1984@mail.ru>
2019-12-22 15:31:25 +0600

    API_service lesson 2 , improvements 2

--- bit_ly.py
+++ bit_ly.py
@@ -2,80 +2,79 @@ import requests
 import argparse
 from urllib.parse import urlsplit
 import os
+
 from dotenv import load_dotenv
+from urllib3.exceptions import HTTPError
 
-load_dotenv()
+PARAMS = {"unit": "day", "units": -1}
+URL_SHORT = 'https://api-ssl.bitly.com/v4/shorten'
 
-TOKEN = os.getenv('TOKEN')
-headers = {"Authorization": f"Bearer {TOKEN}"}
 
-params = {"unit": "day", "units": -1}
-
-url_shorten = 'https://api-ssl.bitly.com/v4/shorten'
-
-parser = argparse.ArgumentParser(description='bitly shorted link')
-parser.add_argument('long_link', type=str)
-args = parser.parse_args()
+def get_parser():
+    parser = argparse.ArgumentParser(description='bitly shorted link')
+    parser.add_argument('long_link', type=str)
+    args = parser.parse_args()
+    return args
 
 
 def get_short(url, headers, data):
-    try:
-        response = requests.post(url, headers=headers, json=data)
-        response.raise_for_status()
-        json_data = response.json()
-        return json_data
-    except requests.exceptions.HTTPError as error:
-        print(f'error \n{error}')
+    response = requests.post(url, headers=headers, json=data)
+    response.raise_for_status()
+    json_data = response.json()
+    return json_data
 
 
 def get_total_clicks(url, headers, params):
-    try:
-        response = requests.get(url, headers=headers, params=params)
-        response.raise_for_status()
-        json_data = response.json()
-        return json_data
-    except requests.exceptions.HTTPError as error:
-        print(f'error \n{error}')
+    response = requests.get(url, headers=headers, params=params)
+    response.raise_for_status()
+    json_data = response.json()
+    return json_data
 
 
 def get__link(data):
     if 'total_clicks' in data:
-        get__link = data['total_clicks']
-        return get__link
+        link = data['total_clicks']
+        return link
     else:
-        get__link = data['link']
-        return get__link
+        link = data['link']
+        return link
 
 
 def check_link(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-    except requests.exceptions.HTTPError as error:
-        print(f'link error: \n{error}')
-        exit()
+    response = requests.get(url)
+    response.raise_for_status()
 
 
-def url_split(url):
+def get_split_url(url):
     base_url = "{0.netloc}{0.path}".format(urlsplit(url))
     return base_url
 
 
 def main():
-    check_link(args.long_link)
+    args = get_parser()
+    get_parser()
+    load_dotenv(verbose=True)
+    token_bitly = os.getenv('TOKEN_BITLY')
+    headers = {"Authorization": f"Bearer {token_bitly}"}
 
-    if 'bit.ly' in args.long_link:
-        bitlink = args.long_link
-        cut_link = url_split(bitlink)
-        url_clicks = f'https://api-ssl.bitly.com/v4/bitlinks/{cut_link}/clicks/summary'
-        json_summ_clicks = get_total_clicks(url_clicks, headers, params)
-        print('')
-        print(f'количество кликов по ссылке: {get__link(json_summ_clicks)}')
-    else:
-        body = {"long_url": args.long_link}
-        short_json = get_short(url_shorten, headers, body)
-        bitlink = get__link(short_json)
-        print(f'Битлинк: {bitlink}')
+    try:
+        check_link(args.long_link)
+
+        if 'bit.ly' in args.long_link:
+            bitlink = args.long_link
+            cropped_link = get_split_url(bitlink)
+            total_clicks_url = f'https://api-ssl.bitly.com/v4/bitlinks/{cropped_link}/clicks/summary'
+            json_summ_clicks = get_total_clicks(total_clicks_url, headers, PARAMS)
+            print('')
+            print(f'количество кликов по ссылке: {get__link(json_summ_clicks)}')
+        else:
+            body = {"long_url": args.long_link}
+            json_short_link = get_short(URL_SHORT, headers, body)
+            bitlink = get__link(json_short_link)
+            print(f'Битлинк: {bitlink}')
+    except HTTPError as error:
+        print(f'{error}')
+        exit()
 
 
 if __name__ == '__main__':
