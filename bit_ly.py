7de4de43b23beaf1691c9eeae7b66dc7859ad8f2
AxmetES <axmet-1984@mail.ru>
2019-12-23 23:28:57 +0600

    API_service lesson 2 , improvements 3

--- bit_ly.py
+++ bit_ly.py
@@ -20,29 +20,24 @@ def get_parser():
 def get_short(url, headers, data):
     response = requests.post(url, headers=headers, json=data)
     response.raise_for_status()
-    json_data = response.json()
-    return json_data
+    data = response.json()
+    return data
 
 
 def get_total_clicks(url, headers, params):
     response = requests.get(url, headers=headers, params=params)
     response.raise_for_status()
-    json_data = response.json()
-    return json_data
+    data = response.json()
+    return data
 
 
-def get__link(data):
+def get_link(data):
     if 'total_clicks' in data:
         link = data['total_clicks']
         return link
     else:
         link = data['link']
         return link
-
-
-def check_link(url):
-    response = requests.get(url)
-    response.raise_for_status()
 
 
 def get_split_url(url):
@@ -58,23 +53,21 @@ def main():
     headers = {"Authorization": f"Bearer {token_bitly}"}
 
     try:
-        check_link(args.long_link)
-
         if 'bit.ly' in args.long_link:
             bitlink = args.long_link
             cropped_link = get_split_url(bitlink)
             total_clicks_url = f'https://api-ssl.bitly.com/v4/bitlinks/{cropped_link}/clicks/summary'
-            json_summ_clicks = get_total_clicks(total_clicks_url, headers, PARAMS)
+            summ_clicks_dict = get_total_clicks(total_clicks_url, headers, PARAMS)
             print('')
-            print(f'количество кликов по ссылке: {get__link(json_summ_clicks)}')
+            print(f'количество кликов по ссылке: {get_link(summ_clicks_dict)}')
         else:
             body = {"long_url": args.long_link}
-            json_short_link = get_short(URL_SHORT, headers, body)
-            bitlink = get__link(json_short_link)
+            short_link_dict = get_short(URL_SHORT, headers, body)
+            bitlink = get_link(short_link_dict)
             print(f'Битлинк: {bitlink}')
     except HTTPError as error:
         print(f'{error}')
-        exit()
+        exit(str(error))
 
 
 if __name__ == '__main__':
